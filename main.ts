aa13ca73ce247255bcffa48cd2f5dbf4be16689e
Sebimala <86785937+Sebimala@users.noreply.github.com>
2024-04-14 11:59:11 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+bitbot.select_model(BBModel.XL)
 basic.forever(function () {
-	
+    if (bitbot.readLine(BBLineSensor.Left) == 1 && bitbot.readLine(BBLineSensor.Right) == 0) {
+        bitbot.rotatems(BBRobotDirection.Left, 100, 400)
+    } else if (bitbot.readLine(BBLineSensor.Left) == 0 && bitbot.readLine(BBLineSensor.Right) == 1) {
+        bitbot.rotatems(BBRobotDirection.Right, 100, 400)
+    } else {
+        bitbot.go(BBDirection.Forward, 60)
+    }
 })

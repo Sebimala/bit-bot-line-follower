d410600a487d74bde0188d71979629af9af8bfe1
Sebimala <86785937+Sebimala@users.noreply.github.com>
2024-04-21 18:01:54 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,8 @@
 input.onButtonPressed(Button.A, function () {
     if (Running == 0) {
         Running = 1
-        basic.showLeds(`
-            . . # . .
-            . # # # .
-            # . # . #
-            . . # . .
-            . . # . .
-            `)
     } else {
         Running = 0
-        basic.showIcon(IconNames.No)
     }
 })
 let Running = 0
@@ -20,12 +12,34 @@ basic.forever(function () {
     if (Running == 1) {
         if (bitbot.readLine(BBLineSensor.Left) == 1 && bitbot.readLine(BBLineSensor.Right) == 0) {
             bitbot.rotatems(BBRobotDirection.Left, 50, 400)
+            basic.showLeds(`
+                . . # . .
+                . # . . .
+                # # # # #
+                . # . . .
+                . . # . .
+                `)
         } else if (bitbot.readLine(BBLineSensor.Left) == 0 && bitbot.readLine(BBLineSensor.Right) == 1) {
             bitbot.rotatems(BBRobotDirection.Right, 50, 400)
+            basic.showLeds(`
+                . . # . .
+                . . . # .
+                # # # # #
+                . . . # .
+                . . # . .
+                `)
         } else {
-            bitbot.go(BBDirection.Forward, 45)
+            bitbot.go(BBDirection.Forward, 15)
+            basic.showLeds(`
+                . . # . .
+                . # # # .
+                # . # . #
+                . . # . .
+                . . # . .
+                `)
         }
     } else {
         bitbot.stop(BBStopMode.Brake)
+        basic.clearScreen()
     }
 })
